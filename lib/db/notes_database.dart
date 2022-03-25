3389ca5ffdda1d3ad6248dca7031861b3a9d7614
Krystian Wiewior <krystian.wiewior@gmail.com>
2022-03-25 17:34:04 +0100

    SQL db and model add

--- lib/db/notes_database.dart
+++ lib/db/notes_database.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+import '../model/note.dart';
+
 class NotesDatabase {
   static final NotesDatabase instance = NotesDatabase._init();
 
@@ -22,7 +24,86 @@ class NotesDatabase {
     return await openDatabase(path, version: 1, onCreate: _createDB);
   }
 
-  Future _createDB(Database db, int version) async {}
+  Future _createDB(Database db, int version) async {
+    final idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    final textType = 'TEXT NOT NULL';
+    final boolType = 'BOOLEAN NOT NULL';
+    final integerType = 'INTEGER NOT NULL';
+
+    await db.execute(''' CREATE TABLE $tableNotes (
+      ${NoteFields.id} $idType, 
+      ${NoteFields.isImportant} $boolType,
+      ${NoteFields.number} $integerType,
+      ${NoteFields.title} $textType,
+      ${NoteFields.description} $textType,
+      ${NoteFields.time} $textType
+    )''');
+  }
+
+  Future<Note> create(Note note) async {
+    final db = await instance.database;
+
+    // final columns = '${NoteFields.title}, ${NoteFields.description}, ${NoteFields.time}';
+    // final values = '${json[NoteFields.title]}, ${json[NoteFields.description], ${json[NoteFields.]}'
+    //
+    // final id = await db
+    // .rawInsert('INSERT INTO table_name ($columns) VALUES ($values');
+    //
+    final id = await db.insert(tableNotes, note.toJson());
+    return note.copy(id: id);
+  }
+
+  Future<Note> readNote(int id) async {
+    final db = await instance.database;
+
+
+    // prevents sql injection attacks = ? instead of = [id]
+    final maps = await db.query(
+      tableNotes,
+      columns: NoteFields.values,
+      where: '${NoteFields.id} = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return Note.fromJson(maps.first);
+    } else {
+      throw Exception('ID $id not found');
+    }
+  }
+
+  Future<List<Note>> readAllNotes() async {
+    final db = await instance.database;
+
+    final orderBy = '${NoteFields.time} ASC';
+    // final result =
+    //     await db.rawQuery('SELECT * FROM $tableNotes ORDER BY $orderBy');
+
+    final result = await db.query(tableNotes, orderBy: orderBy);
+
+    return result.map((json) => Note.fromJson(json)).toList();
+  }
+
+  Future<int> update(Note note) async {
+    final db = await instance.database;
+
+    return db.update(
+      tableNotes,
+      note.toJson(),
+      where: '${NoteFields.id} = ?',
+      whereArgs: [note.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final db = await instance.database;
+
+    return await db.delete(
+      tableNotes,
+      where: '${NoteFields.id} = ?',
+      whereArgs: [id],
+    );
+  }
 
   Future close() async {
     final db = await instance.database;
